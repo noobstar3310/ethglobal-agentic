64cd90be3453e41939483ebbb29fafab440ecc51
Khaw Tze Ern,Celine <157878271+KTECeline@users.noreply.github.com>
2025-02-07 19:28:02 +0800

    connect button

--- src/components/Wallet-button.tsx
+++ src/components/Wallet-button.tsx
@@ -1,4 +1,16 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 export function WalletButton() {
-  return <ConnectButton />;
+  return (
+    <ConnectButton.Custom>
+      {({ account, openConnectModal }) => (
+        <button
+          onClick={openConnectModal}
+          className="px-4 py-2 text-black font-semibold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 opacity-50 rounded-lg shadow-lg transition hover:opacity-60"
+        >
+          {account ? `Connected: ${account.displayName}` : "Connect Wallet"}
+        </button>
+      )}
+    </ConnectButton.Custom>
+  );
 }
